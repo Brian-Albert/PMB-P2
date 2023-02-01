0be27908660ec3cb5517b84107848a9d0da8ea4c
Brian-Albert <97513515+Brian-Albert@users.noreply.github.com>
2023-02-01 12:53:31 +0700

    quiz

--- quiz/app/src/main/java/com/example/quiz/edit.java
+++ quiz/app/src/main/java/com/example/quiz/edit.java
@@ -36,7 +36,7 @@ public class edit extends AppCompatActivity {
         submit2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(edit.this, MainActivity.class);
+                Intent intent = new Intent(edit.this, profil.class);
                 startActivity(intent);
             }
         });
--- quiz/app/src/main/java/com/example/quiz/form.java
+++ quiz/app/src/main/java/com/example/quiz/form.java
@@ -3,12 +3,24 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class form extends AppCompatActivity {
-
+    Button btnSimpan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        btnSimpan = findViewById(R.id.btnSimpan);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(form.this,"Berhasil Disimpan",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
